648a4d02877d21623489b72b9bdde596fc162f74
David Kane <d.r.kane@gmail.com>
2017-04-13 01:05:44 +0100

    Move index creation to a separate file to avoid overwriting every time

---
+++ create_elasticsearch.py
@@ -0,0 +1,32 @@
+import argparse
+from elasticsearch import Elasticsearch
+
+
+INDEXES = [
+    {"name": "postcode", "mapping": ("postcode", {"properties": {"location": {"type": "geo_point"}}}) },
+    {"name": "charitysearch", "mapping": ("charity", {"properties": {"geo": {"properties": {"location": {"type": "geo_point"}}}}}) }
+]
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Setup elasticsearch indexes.')
+    parser.add_argument('--reset', action='store_true',
+                        help='If set, any existing indexes will be deleted and recreated.')
+    args = parser.parse_args()
+
+    es = Elasticsearch()
+
+    for i in INDEXES:
+        if es.indices.exists( i["name"]  ) and args.reset:
+            print("[elasticsearch] deleting '%s' index..." % ( i["name"]  ))
+            res = es.indices.delete(index =  i["name"]  )
+            print("[elasticsearch] response: '%s'" % (res))
+        print("[elasticsearch] creating '%s' index..." % ( i["name"]  ))
+        res = es.indices.create(index =  i["name"]  )
+
+        if "mapping" in i:
+            res = es.indices.put_mapping(i["mapping"][0], i["mapping"][1], index= i["name"]   )
+            print("[elasticsearch] set mapping on %s index" % ( i["name"]  ))
+
+if __name__ == '__main__':
+    main()
