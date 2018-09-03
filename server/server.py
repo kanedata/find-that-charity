4f69e05ace1fc05f4a2dd9340987b07e78bc7e47
David Kane <d.r.kane@gmail.com>
2018-09-03 17:45:36 +0100

    Remove duplicate lines from server

--- server/server.py
+++ server/server.py
@@ -9,8 +9,6 @@ from datetime import datetime, timezone
 import csv
 import io
 from dateutil import parser
-import requests
-from bs4 import BeautifulSoup
 
 import bottle
 from elasticsearch import Elasticsearch
@@ -388,60 +386,6 @@ def autocomplete():
             "value": x["_id"]
         } for x in res.get("suggest", {}).get("suggest-1", [])[0]["options"]
     ]}
-
-@app.route('/feeds/ccew.<filetype>')
-def ccew_rss(filetype):
-    CCEW_URL = 'http://data.charitycommission.gov.uk/'
-    res = requests.get(CCEW_URL)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    items = []
-    for i in soup.find_all('blockquote'):
-        links = i.find_all('a')
-        idate = parser.parse(
-                i.h4.string.split(", ")[1],
-                default=datetime(2018, 1, 12, 0, 0)
-            ).replace(tzinfo=timezone.utc)
-        items.append({
-            "name": i.h4.string,
-            "date": idate,
-            "link": links[0].get('href'),
-            "author": "Charity Commission for England and Wales",
-        })
-
-    feed_contents = dict(
-        items=items, 
-        title='Charity Commission for England and Wales data downloads',
-        description='Downloads available from Charity Commission data downloads page.',
-        url=CCEW_URL,
-        feed_url=bottle.request.url,
-        updated=datetime.now().replace(tzinfo=timezone.utc),
-    )
-
-    if filetype=='atom':
-        bottle.response.content_type = 'application/atom+xml'
-        template='atom.xml'
-    if filetype=="json":
-        bottle.response.content_type = 'application/json'
-        return {
-            "version": "https://jsonfeed.org/version/1",
-            "title": feed_contents["title"],
-            "home_page_url": feed_contents["url"],
-            "feed_url": feed_contents["feed_url"],
-            "description": feed_contents["description"],
-            "items": [
-                {
-                    "id": item["link"],
-                    "url": item["link"],
-                    "title": item["name"],
-                    "date_published": item["date"].isoformat(),
-                } for item in items
-            ]
-        }
-    else:
-        bottle.response.content_type = 'application/rss+xml'
-        template='rss.xml'
-
-    return bottle.template(template, **feed_contents)
 
 
 def get_csv_options():
