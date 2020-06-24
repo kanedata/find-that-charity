60cb13d68976559144c98234257eb072ed23463e
David Kane <david@dkane.net>
2020-06-24 21:47:03 +0100

    yet again

--- findthatcharity/wsgi.py
+++ findthatcharity/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'findthatcharity.findthatcharity.settings')
+                      'findthatcharity.settings')
 
 application = get_wsgi_application()
