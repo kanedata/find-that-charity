47f1e17a576891f8d0a85f5fbe89237f277e4ada
David Kane <david@dkane.net>
2020-06-24 22:04:30 +0100

    settings

--- findthatcharity/settings.py
+++ findthatcharity/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(";")
-print(ALLOWED_HOSTS)
 
 # Application definition
 
