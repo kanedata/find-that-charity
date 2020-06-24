4b6ea0bf5bca99805319c74e1719934a815b1005
David Kane <david@dkane.net>
2020-06-24 21:59:38 +0100

    show settings

--- findthatcharity/settings.py
+++ findthatcharity/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(";")
-
+print(ALLOWED_HOSTS)
 
 # Application definition
 
