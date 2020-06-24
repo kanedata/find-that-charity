b156e5fc7f160db7eb16b11c202b0a2cbcfc4b2b
David Kane <david@dkane.net>
2020-06-24 21:55:34 +0100

    get allowed hosts from env var

--- findthatcharity/settings.py
+++ findthatcharity/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(";")
 
 
 # Application definition
