8da4be8997775ce3bb062544fd6c9781a28abbc6
David Kane <david@dkane.net>
2020-06-26 16:02:51 +0100

    fix whitenoise storage

--- findthatcharity/settings.py
+++ findthatcharity/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', 'true').lower().startswith('t')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(";") if os.environ.get('ALLOWED_HOSTS') else []
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
