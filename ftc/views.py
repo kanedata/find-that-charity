bcb3ef132482fe4e3e0c69eb31b8b5579e9c8257
David Kane <david@dkane.net>
2020-08-06 09:15:31 +0100

    better caching

--- ftc/views.py
+++ ftc/views.py
@@ -1,10 +1,10 @@
 import csv
 
+from django.core.cache import cache
 from django.db.models import Count, F, Func
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views.decorators.cache import cache_page
 from django.conf import settings
 
 from ftc.documents import FullOrganisation
@@ -15,38 +15,43 @@ from ftc.query import OrganisationSearch, random_query
 
 
 # site homepage
-@cache_page(60 * 60)
 def index(request):
     if "q" in request.GET:
         return org_search(request)
 
-    orgs = Organisation.objects
+    cache_key = 'homepage_context'
+    context = cache.get(cache_key)
 
-    by_orgtype = (
-        orgs.annotate(orgtype=Func(F("organisationType"), function="unnest"))
-        .values("orgtype")
-        .annotate(records=Count("*"))
-        .order_by("-records")
-    )
-    by_source = (
-        Source.objects.all()
-        .annotate(records=Count("organisations"))
-        .order_by("-records")
-    )
+    if not context:
 
-    context = dict(
-        examples={
-            "registered-charity-england-and-wales": "GB-CHC-1177548",
-            "registered-charity-scotland": "GB-SC-SC007427",
-            "registered-charity-northern-ireland": "GB-NIC-104226",
-            "community-interest-company": "GB-COH-08255580",
-            "local-authority": "GB-LAE-IPS",
-            "universities": "GB-EDU-133808",
-        },
-        term="",
-        by_orgtype=by_orgtype,
-        by_source=by_source,
-    )
+        orgs = Organisation.objects
+
+        by_orgtype = (
+            orgs.annotate(orgtype=Func(F("organisationType"), function="unnest"))
+            .values("orgtype")
+            .annotate(records=Count("*"))
+            .order_by("-records")
+        )
+        by_source = (
+            Source.objects.all()
+            .annotate(records=Count("organisations"))
+            .order_by("-records")
+        )
+
+        context = dict(
+            examples={
+                "registered-charity-england-and-wales": "GB-CHC-1177548",
+                "registered-charity-scotland": "GB-SC-SC007427",
+                "registered-charity-northern-ireland": "GB-NIC-104226",
+                "community-interest-company": "GB-COH-08255580",
+                "local-authority": "GB-LAE-IPS",
+                "universities": "GB-EDU-133808",
+            },
+            term="",
+            by_orgtype=by_orgtype,
+            by_source=by_source,
+        )
+        cache.set(cache_key, context, 60 * 60)
     return render(request, "index.html.j2", context)
 
 
