3f11984afc4e08381bd8af9df17dc8b8c5bc81d9
David Kane <david@dkane.net>
2021-09-02 19:32:15 +0100

    fix for redirect (issue #102)

--- charity/tests.py
+++ charity/tests.py
@@ -6,7 +6,12 @@ from ftc.tests import TestCase
 class CharityViewTests(TestCase):
     def test_charity(self):
 
-        response = self.client.get(reverse("charity_html", kwargs={"regno": "1234"}))
+        response = self.client.get(
+            reverse("charity_html", kwargs={"regno": "1234"}), follow=True
+        )
+        self.assertRedirects(
+            response, "/orgid/GB-CHC-1234", status_code=301, target_status_code=200
+        )
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Find that Charity", html=True)
         self.assertContains(response, "Source publisher", html=True)
@@ -16,5 +21,10 @@ class CharityViewTests(TestCase):
 
     def test_organisation_404(self):
 
-        response = self.client.get(reverse("charity_html", kwargs={"regno": "3456"}))
+        response = self.client.get(
+            reverse("charity_html", kwargs={"regno": "3456"}), follow=True
+        )
+        self.assertRedirects(
+            response, "/orgid/GB-CHC-3456", status_code=301, target_status_code=404
+        )
         self.assertEqual(response.status_code, 404)
--- charity/views.py
+++ charity/views.py
@@ -15,6 +15,6 @@ def get_charity(request, regno, filetype="html", preview=False):
         org_id = "GB-CHC-{}".format(regno)
 
     if filetype == "html":
-        return redirect("orgid_html", kwargs=dict(org_id=org_id), permanent=True)
+        return redirect("orgid_html", org_id=org_id, permanent=True)
 
     return get_org_by_id(request, org_id, filetype, preview, as_charity=True)
