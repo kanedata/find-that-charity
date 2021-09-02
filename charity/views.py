8890ee0f5cd81e184dab661a1271faa403a59b9b
David Kane <david@dkane.net>
2021-09-02 19:22:26 +0100

    redirect to orgid view (fixes #102)

--- charity/views.py
+++ charity/views.py
@@ -1,6 +1,6 @@
 import re
 
-from django.http import Http404
+from django.shortcuts import redirect
 
 from ftc.views import get_org_by_id
 
@@ -8,17 +8,13 @@ from ftc.views import get_org_by_id
 def get_charity(request, regno, filetype="html", preview=False):
     regno = regno.strip().upper()
     if regno.startswith("SC"):
-        org_ids = ["GB-SC-{}".format(regno)]
-    elif regno.startswith("N"):
-        org_ids = ["GB-NIC-{}".format(re.sub("[^0-9]", "", regno))]
+        org_id = "GB-SC-{}".format(regno)
+    elif regno.startswith("N") or (regno.startswith("1") and len(regno) == 6):
+        org_id = "GB-NIC-{}".format(re.sub("[^0-9]", "", regno))
     else:
-        org_ids = [
-            "GB-CHC-{}".format(regno),
-            "GB-NIC-{}".format(regno),
-        ]
-    for org_id in org_ids:
-        try:
-            return get_org_by_id(request, org_id, filetype, preview, as_charity=True)
-        except Http404:
-            continue
-    raise Http404("Charity not found")
+        org_id = "GB-CHC-{}".format(regno)
+
+    if filetype == "html":
+        return redirect("orgid_html", kwargs=dict(org_id=org_id), permanent=True)
+
+    return get_org_by_id(request, org_id, filetype, preview, as_charity=True)
