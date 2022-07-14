b66faf21ba3c9db0c43aafbfc9f02818caf2b218
David Kane <david@dkane.net>
2022-07-14 16:23:31 +0100

    reduce number of transactions sent to sentry

--- findthatcharity/settings.py
+++ findthatcharity/settings.py
@@ -31,7 +31,7 @@ if os.environ.get("SENTRY_DSN"):
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
-        traces_sample_rate=0.05,
+        traces_sample_rate=0.005,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True,
