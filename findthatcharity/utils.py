c579fcaa41b8e0bca479a4e3191d52f5381187a2
David Kane <david@dkane.net>
2022-08-12 14:49:51 +0100

    add utility functions

--- findthatcharity/utils.py
+++ findthatcharity/utils.py
@@ -1,8 +1,12 @@
 import re
+from urllib.parse import urlparse
 
 import babel.numbers
 import inflect
 import titlecase
+from django.utils.text import slugify
+
+from findthatcharity.settings import IGNORE_DOMAINS
 
 VOWELS = re.compile("[AEIOUYaeiouy]")
 ORD_NUMBERS_RE = re.compile(r"([0-9]+(?:st|nd|rd|th))")
@@ -185,3 +189,22 @@ def str_format(value, format="{}"):
 
 def a_or_an(value):
     return p.a(value).split()[0]
+
+
+def normalise_name(n):
+    stopwords = ["the", "of", "in", "uk", "ltd", "limited"]
+    n = slugify(n)
+    return " ".join([w for w in n.split("-") if w not in stopwords])
+
+
+def get_domain(url):
+    if not url:
+        return None
+    if not url.startswith("http"):
+        url = "http://" + url
+    domain = urlparse(url).netloc
+    if domain.startswith("www."):
+        domain = domain[4:]
+    if domain in IGNORE_DOMAINS:
+        return "IGNORED {}".format(url)
+    return domain
