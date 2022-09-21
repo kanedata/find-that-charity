4fc5e3e5fedb8afa23bf6d33aa55e87a5d93ae6b
David Kane <david@dkane.net>
2022-09-21 15:09:53 +0100

    fix sort name utils

--- ftc/tests/test_documents.py
+++ ftc/tests/test_documents.py
@@ -9,17 +9,20 @@ from ftc.models import Organisation
 class TestDocuments(TestCase):
     def test_fullorganisation_sortname(self):
         names = (
-            ("the charity the name", "charity the name"),
-            ("charity the name", "charity the name"),
-            (" the charity the name", "charity the name"),
-            ("the  charity the name", "charity the name"),
-            ("' charity the name'", "charity the name"),
-            ("'  charity the name'", "charity the name"),
-            (" charity the name", "charity the name"),
-            ("(2charity the name", "2charity the name"),
+            ("the charity the name", "charity name"),
+            ("charity the name", "charity name"),
+            (" the charity the name", "charity name"),
+            ("the  charity the name", "charity name"),
+            ("' charity the name'", "charity name"),
+            ("'  charity the name'", "charity name"),
+            ("-  charity the name'", "charity name"),
+            (" charity the name", "charity name"),
+            ("(2charity the name", "2charity name"),
+            ("1st borough scouts", "1st borough scouts"),
             ("Above Us Only Sky?", "above us only sky"),
         )
         for n1, n2 in names:
             o = Organisation(name=n1)
             d = OrganisationGroup()
-            assert d.prepare_sortname(o) == n2
+            with self.subTest(n1=n1, n2=n2):
+                assert d.prepare_sortname(o) == n2
