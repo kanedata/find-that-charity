10164850f7f2ce51d578877ea28d49a54935566f
David Kane <david@dkane.net>
2023-02-04 09:55:52 +0000

    add sql file

---
+++ .github/initdb/create_dbs.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE github_actions_data;
+CREATE DATABASE github_actions_admin;
+GRANT ALL PRIVILEGES ON DATABASE github_actions_data TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE github_actions_admin TO postgres;
